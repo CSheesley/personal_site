57ae40db44d695418d390b35e20175525fcf9e3f
Corey Sheesley <corey.sheesley@gmail.com>
2019-03-01 17:38:22 -0700

    Updates

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -5,6 +5,8 @@ class PersonalSite
     case env["PATH_INFO"]
     when '/' then index
     when '/about' then about
+    when '/main.css' then css
+    when '/error.css' then css_red
     else
       error
     end
@@ -24,5 +26,17 @@ class PersonalSite
 
   def self.render_view(page, code = '200')
     [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
+  end
+
+  def self.css
+    render_static('main.css')
+  end
+
+  def self.css_red
+    render_static('error.css')
+  end
+
+  def self.render_static(asset)
+    [200, {'Content-Type' => 'text/html'}, [File.read("./public/#{asset}")]]
   end
 end
